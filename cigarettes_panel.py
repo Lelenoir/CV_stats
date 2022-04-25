205b5df4c2ac49b5fc5172b613cc4ce5b8dce486
Elena Gubina <e.gubina@napoleonit.com>
2022-04-25 17:19:37 +0500

    add

--- cigarettes_panel.py
+++ cigarettes_panel.py
@@ -145,7 +145,7 @@ total_stats = alt.Chart(pivot_days).mark_line().encode(
         scale=alt.Scale(reverse=False)
     ),
     color=alt.datum(alt.repeat("layer")),
-).repeat(layer=["count_cv_full_rec:Q", "count_mon_full_rec:Q", "count_full_intersection:Q"])
+).repeat(layer=["count_cv_full_rec", "count_mon_full_rec", "count_full_intersection"])
 
 # y=['count_cv_full_rec:Q', "count_mon_full_rec:Q", "count_full_intersection:Q"],
 # tooltip=['dt_create', 'count_cv_full_rec', "count_mon_full_rec", "count_full_intersection"]
