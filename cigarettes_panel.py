1fac9e196030856d8ef72101bdeb862f7f2c803e
Elena Gubina <e.gubina@napoleonit.com>
2022-04-26 12:12:55 +0500

    add

--- cigarettes_panel.py
+++ cigarettes_panel.py
@@ -441,12 +441,16 @@ a = alt.Chart(in_monita[(in_monita.shop_network_name == selected_sn) & (in_monit
 
 b = alt.Chart(in_monita[(in_monita.shop_network_name == selected_sn) & (in_monita.dt_create == choise_date.strftime("%m-%d"))]).mark_circle().encode(
     #  x='count_cv_full_rec', y='count_mon_full_rec', size='recognition', color='recognition', tooltip=['count_cv_full_rec', 'count_mon_full_rec', 'recognition'])
+     x='is_manual', y='count_mon_full_rec', tooltip=['is_manualad', 'count_mon_full_rec'])
+
+c = alt.Chart(in_monita[(in_monita.shop_network_name == selected_sn) & (in_monita.dt_create == choise_date.strftime("%m-%d"))]).mark_circle().encode(
+    #  x='count_cv_full_rec', y='count_mon_full_rec', size='recognition', color='recognition', tooltip=['count_cv_full_rec', 'count_mon_full_rec', 'recognition'])
      x='count_full_intersection', y='count_mon_full_rec', tooltip=['count_full_intersection', 'count_mon_full_rec'])
 
 coll, coll2, coll3 = st.columns([1,1,1])
 coll.altair_chart(a, use_container_width=True)
 coll2.altair_chart(b, use_container_width=True)
-coll2.altair_chart("")
+coll3.altair_chart(c, use_container_width=True)
 
 form = st.form(key="Filtration by shop_network")
 
