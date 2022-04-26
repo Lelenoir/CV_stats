29451d95011ac02cb5b174551115d11e9fb5beb1
Elena Gubina <e.gubina@napoleonit.com>
2022-04-26 10:18:39 +0500

    add

--- cigarettes_panel.py
+++ cigarettes_panel.py
@@ -433,7 +433,7 @@ col_one_sn.table(
 
 col_share.write("")
 
-c = alt.Chart(df[(df.shop_network_name == selected_sn) & (df.dt_create == choise_date.strftime("%m-%d"))]).mark_circle().encode(
+c = alt.Chart(in_monita[(in_monita.shop_network_name == selected_sn) & (in_monita.dt_create == choise_date.strftime("%m-%d"))]).mark_circle().encode(
      x='count_cv_full_rec', y='count_mon_full_rec', size='recognition', color='recognition', tooltip=['count_cv_full_rec', 'count_mon_full_rec', 'recognition'])
 
 st.altair_chart(c, use_container_width=True)
